abbb2ac51544e1bad1cd777a255d811f39d09de6
Miriam Castillo <castillo.m.mc@gmail.com>
2018-05-05 03:52:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     line_is = "The line is currently: "
-    katz_deli.each.with_index(1) do |name|
+    katz_deli.each.with_index(1) do |name, index|
 
     end
 end
