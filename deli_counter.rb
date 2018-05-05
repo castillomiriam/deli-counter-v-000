b34785bc74a16eb9053d506c9b672119cbbb448c
Miriam Castillo <castillo.m.mc@gmail.com>
2018-05-05 04:09:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,4 +12,4 @@ end
 
 def take_a_number(array, name)
   array.push(name)
-  puts "Welcome, #{name} You are number #{array.length} in line."
+  puts "Welcome, #{name}. You are number #{array.length} in line."
